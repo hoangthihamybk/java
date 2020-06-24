5833a0930780ca4a2ab7a3821620f7e4ca634c4d
my-hoang <my-hoang@dimage.co.jp>
2020-06-24 14:18:51 +0700

    demo restfulapi

---
+++ restfulapi/src/main/java/com/example/demo/entity/User.java
@@ -0,0 +1,28 @@
+package com.example.demo.entity;
+
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "users")
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private long id;
+
+    @Column(name = "first_name")
+    private String firstName;
+
+    @Column(name = "last_name")
+    private String lastName;
+
+    @Column(name = "email")
+    private String email;
+}
